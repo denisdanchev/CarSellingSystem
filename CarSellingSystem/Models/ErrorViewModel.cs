f68134303171abf6c99d9d3a69a4329e09b030ed
Denis Danchev <denisdanchev982@gmail.com>
2024-03-18 17:51:34 +0200

    Add project files.

---
+++ CarSellingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarSellingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
