c78a1d3110cbb34f5145495d640d6aa23bee7f92
Denis Danchev <denisdanchev982@gmail.com>
2024-03-20 19:27:18 +0200

    Home page implemented

--- CarSellingSystem/Controllers/HomeController.cs
+++ CarSellingSystem/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using CarSellingSystem.Core.Models.Home;
+﻿using CarSellingSystem.Core.Contracts;
+using CarSellingSystem.Core.Models.Home;
 using CarSellingSystem.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,15 +9,18 @@ namespace CarSellingSystem.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IVehicleService vehicleService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+            IVehicleService _vehicleservice)
         {
             _logger = logger;
+            vehicleService = _vehicleservice;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var model = new IndexViewModel();
+            var model = await vehicleService.LastThreeVehicle();
 
             return View(model);
         }
