c4d58ea34f2b4635d0239801346ecec0287caaec
Denis Danchev <denisdanchev982@gmail.com>
2024-03-25 18:20:15 +0200

    Message constats added

--- CarSellingSystem.Core/Constants/MessageConstants.cs
+++ CarSellingSystem.Core/Constants/MessageConstants.cs
@@ -4,6 +4,8 @@
     {
         public const string RequiredMessage = "The Field {0} is required!";
         public const string LengthMessage = "The Field {0} must be between {2} and {1} characters long!";
+        public const string PhoneExists = "Phone number already exist!";
+        public const string HasSells = "You should have no sells to become a seller!";
 
     }
 }
